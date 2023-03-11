ef3b6fce2f0c13871aa73d1917241a1035af4f07
YUSIYUAN <1164818708@qq.com>
2023-03-11 20:38:16 +0800

    - get external content

--- Source/ShootThemUp/Public/Player/STUBaseCharacter.h
+++ Source/ShootThemUp/Public/Player/STUBaseCharacter.h
@@ -6,24 +6,32 @@
 #include "GameFramework/Character.h"
 #include "STUBaseCharacter.generated.h"
 
+class UCameraComponent;
+
 UCLASS()
 class SHOOTTHEMUP_API ASTUBaseCharacter : public ACharacter
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
-	ASTUBaseCharacter();
+    // Sets default values for this character's properties
+    ASTUBaseCharacter();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    UCameraComponent* CameraComponent;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+    void MoveForward(float Amount);
+    void MoveRight(float Amount);
 };
